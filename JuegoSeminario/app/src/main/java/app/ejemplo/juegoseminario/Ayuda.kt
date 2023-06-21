db3fdc96821c5ff0cdc6a7d4f6d972a3968a27a6
Nahuel <91756222+Nahuelbutti@users.noreply.github.com>
2023-06-21 13:18:05 -0300

    Add files via upload

---
+++ JuegoSeminario/app/src/main/java/app/ejemplo/juegoseminario/Ayuda.kt
@@ -0,0 +1,22 @@
+package app.ejemplo.juegoseminario
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class Ayuda : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ayuda)
+
+        supportActionBar?.hide() // borra la barra de arriba
+
+        val AyudaBoton = findViewById<Button>(R.id.botonAtras)
+        AyudaBoton.setOnClickListener {
+            onBackPressed()}
+
+    }
+}
